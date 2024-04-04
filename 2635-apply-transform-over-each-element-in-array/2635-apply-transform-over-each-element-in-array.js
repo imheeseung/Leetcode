591d20cc8f4d311fbfc3ddbc9fddcff93181dca2
imheeseung <115441816+imheeseung@users.noreply.github.com>
2024-04-05 01:41:16 +0900

    Time: 59 ms (14.83%), Space: 49 MB (31.92%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+const map = function(arr, fn) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+
+    return result;
+};
